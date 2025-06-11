18e239b05231f77f7b9e7963515d20f3e2c45d88
GLBRT <ftrinschek@gmail.com>
2025-06-11 20:16:07 +0200

    Add project files.

---
+++ SwapPlaces.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>  
+#include <string>  
+#include <algorithm>
+
+int main()  
+{  
+    std::string stringReverse;  
+    std::cout << "Enter text you want to reverse: ";  
+    std::getline(std::cin, stringReverse);   
+    std::reverse(stringReverse.begin(), stringReverse.end());  
+ 
+    std::cout << stringReverse;  
+
+    return 0;  
+}
